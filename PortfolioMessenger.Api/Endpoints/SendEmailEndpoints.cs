fb1f843c0a86da54c5615880534dd5828440b8cc
BrianKiarieMwaniki <briank.mwaniki@gmail.com>
2024-09-24 14:19:49 +0300

    added docker support

--- PortfolioMessenger.Api/Endpoints/SendEmailEndpoints.cs
+++ PortfolioMessenger.Api/Endpoints/SendEmailEndpoints.cs
@@ -1,6 +1,3 @@
-using System.Text.RegularExpressions;
-using Microsoft.AspNetCore.Http.HttpResults;
-using Microsoft.AspNetCore.Mvc;
 using PortfolioMessenger.Api.Services;
 
 namespace PortfolioMessenger.Api.Endpoints;
